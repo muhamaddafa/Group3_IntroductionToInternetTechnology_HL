84d553642cce37a1484d85b3da74fbee2b11ca54
Rilim06 <r.marvinlim@gmail.com>
2023-04-04 16:40:23 +0700

    Loading transition

--- Script/home.js
+++ Script/home.js
@@ -2,10 +2,18 @@ let bgmusic = $("#bgmusic")[0];
 bgmusic.play();
 bgmusic.volume = 0.35;
 
-$('#btn').click(function() {
-    window.location.href = 'play.html';
+$('#btn').click(function(event) {
+	event.preventDefault(); 
+	$(".mainDisplay").css("background-image", "none");
+	$(".mainDisplay").css("background-color", "black");
+	$(".container").hide();
+	$(".load").show();
+    setTimeout(function() {
+    window.location.href = "play.html"; 
+    }, 5000); 
 });
 
+const spans = document.querySelectorAll('h2 span');
 
 $('#btn2').click(function() {
     window.location.href = 'about.html';
